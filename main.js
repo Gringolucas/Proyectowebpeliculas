6ad714c4577308f9a941ea10959bf2864f65768b
Gringolucas <lucastrucco83@gmail.com>
2023-05-17 18:48:44 -0300

    Se valido edad en js (se agitgrego funcion)

--- main.js
+++ main.js
@@ -51,6 +51,22 @@ function validarFormulario(evento) {
     alert("El email NO es correcto");
     return;
   }
+  function calcularEdad(fecha_nacimiento) {
+    var hoy = new Date();
+    var cumpleanos = new Date(fecha_nacimiento);
+    var edad = hoy.getFullYear() - cumpleanos.getFullYear();
+    var m = hoy.getMonth() - cumpleanos.getMonth();
+    if (m < 0 || (m === 0 && hoy.getDate() < cumpleanos.getDate())) {
+        edad--;
+    }
+    return edad;
+  }
+    var edad = calcularEdad("2000/08/10");
+    if(edad >= 18){
+    alert("Eres mayor de edad :D ");
+    }else{
+    alert("Eres menor de edad :( ");
+    }
   this.submit();
   alert("Muchas gracias por enviar el formulario");
    	document.fvalida.submit();
